9a8c98a937a27ec4238c5047d9fffa9484c7ba13
Matt Sam <MSTTmm1@gmail.com>
2021-08-17 09:31:19 -0400

    ...

--- AI_M_BOT(opencv+yolo)/AI_M_BOT_X0.py
+++ AI_M_BOT(opencv+yolo)/AI_M_BOT_X0.py
@@ -651,7 +651,7 @@ def show_frames(output_pipe, array):
             show_str3 = 'Fire rate is at ' + str('{:02.0f}'.format((10000 / (array[13] + 306)))) + ' RPS'
             show_str4 = 'Please enjoy coding ^_^' if array[17] else 'Please enjoy coding @_@'
             if show_img.any():
-                show_img = cv2.resize(show_img, (array[5], int(array[5] * 6 / 7)))
+                show_img = cv2.resize(show_img, (array[5], int(array[5] * 6 / 7)), interpolation=cv2.INTER_AREA)
                 img_ex = cv2.resize(img_ex, (array[5], int(array[5] / 2)))
                 cv2.putText(show_img, show_str0, (int(array[5] / 25), int(array[5] * 6 / 7 / 12)), font, array[5] / 600, (127, 255, 0), 2, cv2.LINE_AA)
                 cv2.putText(img_ex, show_str1, (10, int(array[5] / 9)), font, array[5] / 450, show_color, 1, cv2.LINE_AA)
@@ -812,7 +812,7 @@ if __name__ == '__main__':
 
         try:
             if screenshot.any():
-                resized_screenshot = cv2.resize(screenshot, (224, 192))
+                resized_screenshot = cv2.resize(screenshot, (224, 192), interpolation=cv2.INTER_AREA)
             arr[5] = (150 if win_cap.get_window_left() - 10 < 150 else win_cap.get_window_left() - 10)
         except (AttributeError, pywintypes.error):
             break
