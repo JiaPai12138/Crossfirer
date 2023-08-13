15f7d955f6acc5ec51909882d9e19f929fa0bd16
WhatEverNames <93147937+AI-M-BOT@users.noreply.github.com>
2023-08-13 16:40:37 -0400

    但愿修复bat编码问题

--- Crossfirer_v2.x/关闭TX残留进程.bat
+++ Crossfirer_v2.x/关闭TX残留进程.bat
@@ -1,4 +1,11 @@
 ::关闭游戏残留进程
+
+net file 1>NUL 2>NUL
+if not '%errorlevel%' == '0' (
+    powershell Start-Process -FilePath "%0" -ArgumentList "%cd%" -verb runas >NUL 2>&1
+    exit /b
+)
+
 taskkill /IM GameLoader.exe /F
 taskkill /IM TQMCenter.exe /F
 taskkill /IM TenioDL.exe /F
